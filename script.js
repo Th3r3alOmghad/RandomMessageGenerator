30284b52ce680e8570dad0df723785b1190ce6cd
Th3r3alOmghad <150550177+Th3r3alOmghad@users.noreply.github.com>
2023-11-24 16:22:08 -0500

    Update script.js
    if else test

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('I am a node program');
+const messages = {
+    jokes:["Whats is red and bad for your teeth? A Brick", "What side of the turkey has more feathers? The Inside."],
+    script:["Good to see you", "Welcome Back!", "Do you ever take a day off?"]
+};
+function messageAtRandom() {
+const randomMessage = messages.length[Math.floor(Math.random() * messages.length)];
+    if (randomMessage === 0){
+        return messages.jokes[Math.floor(Math.random() * messages.jokes.length)];
+    }
+    else{
+        return messages.script[Math.floor(Math.random() * messages.script.length)];
+    }
+}
+console.log(messageAtRandom());
