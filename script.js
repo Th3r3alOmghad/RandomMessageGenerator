597c5097ac0652391a1db3ecb7a2e2cc2957575f
Th3r3alOmghad <150550177+Th3r3alOmghad@users.noreply.github.com>
2023-11-25 09:54:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const messages = {
-    message: [
-     ["Whats is red and bad for your teeth? A Brick", "What side of the turkey has more feathers? The Inside."],
-     ["Why are you here?", "Workaholoc much?", "Do you ever take a day off?"],
-     ["You are amazing", "Thank you for coming to work!", "You are a god among men!"]
-    ]
+    jokes:["Whats is red and bad for your teeth? A Brick", "What side of the turkey has more feathers? The Inside."],
+    tease:["Why are you here?", "Workaholoc much?", "Do you ever take a day off?"],
+    praise:["You are amazing", "Thank you for coming to work!", "You are a god among men!"]
 };
 
-function randomArray(){
-const firstIndex = messages.message[Math.floor(Math.random()* messages.message.length)];
-let chooseArray1 = firstIndex;
-//const secondIndex = messages.message[chooseArray1][Math.floor(Math.random()*messages.messages[chooseArray1])];
-//let chooseArray2 = secondIndex.length;
-let finalChoice = messages.message[chooseArray1];//[chooseArray2];
-console.log(chooseArray1);
-return finalChoice;
+function randomMessage(){
+const arrayCount = for(let properties in messages){
+    count = count +1
+    console.log(count);
+}
+/*const chooseArray = Math.floor(Math.random()*Count(messages));
+const chooseJokes = Math.floor(Math.random()*messages.jokes.length);
+const chooseTease = Math.floor(Math.random()*messages.tease.length);
+const choosePraise = Math.floor(Math.random()*messages.praise.length);
+console.log(arrayCount,chooseJokes,choosePraise,chooseTease);
+
 }
 
-console.log(randomArray())
+*/
+randomMessage();
