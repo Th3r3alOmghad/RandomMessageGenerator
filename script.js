76184685014a2bdeba311b1ca2c685af4936f43a
Th3r3alOmghad <150550177+Th3r3alOmghad@users.noreply.github.com>
2023-11-25 11:21:21 -0500

    Update script.js
    Finally figured it out

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const messages = {
-    jokes:["Whats is red and bad for your teeth? A Brick", "What side of the turkey has more feathers? The Inside."],
-    tease:["Why are you here?", "Workaholoc much?", "Do you ever take a day off?"],
-    praise:["You are amazing", "Thank you for coming to work!", "You are a god among men!"]
+    jokes: ["Whats is red and bad for your teeth? A Brick", "What side of the turkey has more feathers? The Inside."],
+    tease: ["Why are you here?", "Workaholic much?", "Do you ever take a day off?"],
+    praise: ["You are amazing", "Thank you for coming to work!", "You are a god among men!"]
 };
 
-function randomMessage(){
-const arrayCount = for(let properties in messages){
-    count = count +1
-    console.log(count);
+function getRandomMessage() {
+    // Get an array of category keys (jokes, tease, praise)
+    const categories = Object.keys(messages);
+    
+    // Pick a random category from the list
+    const randomCategory = categories[Math.floor(Math.random() * categories.length)];
+    
+    // Get the array associated with the randomly selected category
+    const selectedMessages = messages[randomCategory];
+    
+    // Pick a random message from the selected category
+    const randomMessage = selectedMessages[Math.floor(Math.random() * selectedMessages.length)];
+    
+    return randomMessage;
 }
-/*const chooseArray = Math.floor(Math.random()*Count(messages));
-const chooseJokes = Math.floor(Math.random()*messages.jokes.length);
-const chooseTease = Math.floor(Math.random()*messages.tease.length);
-const choosePraise = Math.floor(Math.random()*messages.praise.length);
-console.log(arrayCount,chooseJokes,choosePraise,chooseTease);
 
-}
-
-*/
-randomMessage();
+// Example usage:
+const randomMessage = getRandomMessage();
+console.log(randomMessage); // This will output a random message from any category
