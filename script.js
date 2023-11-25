b17c77d5e06ff05fe2755443448727c785f596e6
Th3r3alOmghad <150550177+Th3r3alOmghad@users.noreply.github.com>
2023-11-24 19:03:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const messages = {
     message: [
      ["Whats is red and bad for your teeth? A Brick", "What side of the turkey has more feathers? The Inside."],
-     ["Good to see you", "Welcome Back!", "Do you ever take a day off?"],
+     ["Why are you here?", "Workaholoc much?", "Do you ever take a day off?"],
      ["You are amazing", "Thank you for coming to work!", "You are a god among men!"]
     ]
 };
 
+function randomArray(){
 const firstIndex = messages.message[Math.floor(Math.random()* messages.message.length)];
-const secondIndex = messages.messages[firstIndex][Math.floor(Math.random()*messages.messages[firstIndex])];
+let chooseArray1 = firstIndex;
+//const secondIndex = messages.message[chooseArray1][Math.floor(Math.random()*messages.messages[chooseArray1])];
+//let chooseArray2 = secondIndex.length;
+let finalChoice = messages.message[chooseArray1];//[chooseArray2];
+console.log(chooseArray1);
+return finalChoice;
+}
+
+console.log(randomArray())
